a1ad1613e0acd429bede1c822d66252a1280fab6
J <jwj4016@gmail.com>
2023-03-29 13:42:41 +0900

    Initial commit

---
+++ src/main/java/com/example/learing_redis/LearingRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.learing_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearingRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearingRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learing_redis/LearingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learing_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
